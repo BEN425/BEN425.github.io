0bbaa0a4f4f81876b9128c37abf5aef0a67b7613
BEN425 <78299515+BEN425@users.noreply.github.com>
2023-09-30 12:07:15 +0800

    modified:   src/pages/race.js modified:   src/pages/support_card.js modified:   src/styles/RacePage.module.css modified:   src/styles/SingleRace.module.css modified:   src/styles/globals.css

--- src/pages/race.js
+++ src/pages/race.js
@@ -4,9 +4,39 @@ import { useState, useEffect } from "react";
 import styles from "@/styles/RacePage.module.css";
 import { useRouter } from "next/router";
 
+import * as mylib from "../lib";
+
+const gradeList = ["G1", "G2", "G3", "OP", "Pre-OP"];
+const directionList = ["順時針", "逆時針", "直線"];
+const courseList = ["草地", "沙地"];
+const distanceList = ["短距離", "一哩", "中距離", "長距離", "主要距離", "非主要距離"];
+const placeList = [
+	"札幌",
+	"函館",
+	"新潟",
+	"福島",
+	"中山",
+	"東京",
+	"中京",
+	"京都",
+	"阪神",
+	"小倉",
+	"大井",
+	"川崎",
+	"船橋",
+	"盛岡",
+	"隆尚",
+];
+
 export default function RacePage() {
 	const router = useRouter();
 	const [loading, setLoading] = useState(true);
+	// Filters
+	const [gradeFilter, setGradeFilter] = useState(Array(5).fill(false));
+	const [directionFilter, setDirectionFilter] = useState(Array(3).fill(false));
+	const [courseFilter, setCourseFilter] = useState(Array(2).fill(false));
+	const [distanceFilter, setDistanceFilter] = useState(Array(6).fill(false));
+	const [placeFilter, setPlaceFilter] = useState(Array(15).fill(false));
 	// Load race json file
 	const [raceList, setRaceList] = useState({});
 	async function fetchJson() {
@@ -24,7 +54,43 @@ export default function RacePage() {
 	}, []);
 
 	function getItem(item) {
-		return (
+		let matchGrade = !gradeFilter.includes(true);
+		let matchDirection = !directionFilter.includes(true);
+		let matchCourse = !courseFilter.includes(true);
+		let matchDistance = !distanceFilter.includes(true);
+		let matchPlace = !placeFilter.includes(true);
+
+		// Check filters
+		gradeList.forEach((value, index) => {
+			if (gradeFilter[index] && item.basic.grade == value) matchGrade = true;
+		});
+		directionList.forEach((value, index) => {
+			if (directionFilter[index] && item.basic.direction == value) matchDirection = true;
+		});
+		courseList.forEach((value, index) => {
+			if (courseFilter[index] && item.basic.course == value) matchCourse = true;
+		});
+		if (distanceFilter[0] && parseInt(item.basic.distance) <= 1400) matchDistance = true;
+		if (
+			distanceFilter[1] &&
+			1500 <= parseInt(item.basic.distance) &&
+			parseInt(item.basic.distance) <= 1900
+		)
+			matchDistance = true;
+		if (
+			distanceFilter[2] &&
+			2000 <= parseInt(item.basic.distance) &&
+			parseInt(item.basic.distance) <= 2400
+		)
+			matchDistance = true;
+		if (distanceFilter[3] && parseInt(item.basic.distance) >= 2500) matchDistance = true;
+		if (distanceFilter[4] && parseInt(item.basic.distance) % 400 == 0) matchDistance = true;
+		if (distanceFilter[5] && parseInt(item.basic.distance) % 400 != 0) matchDistance = true;
+		placeList.forEach((value, index) => {
+			if (placeFilter[index] && item.basic.place.includes(value)) matchPlace = true;
+		});
+
+		return matchGrade && matchDirection && matchCourse && matchDistance && matchPlace ? (
 			<RaceItem
 				race={item}
 				onClick={() => {
@@ -35,7 +101,7 @@ export default function RacePage() {
 						},
 					});
 				}}></RaceItem>
-		);
+		) : null;
 	}
 
 	return (
@@ -61,6 +127,32 @@ export default function RacePage() {
 					{/* Search Table */}
 					<div className={styles.searchTable}>
 						<div className={styles.grid_container}>
+							<div className={styles.grid_head}>等級</div>
+							{generateGradeButtons(gradeFilter, setGradeFilter)}
+							<div className={styles.grid_head}>方向</div>
+							{generateDirectionButtons(directionFilter, setDirectionFilter)}
+							<div></div>
+							<div></div>
+							<div className={styles.grid_head}>場地</div>
+							{generateCourseButtons(courseFilter, setCourseFilter)}
+							<div></div>
+							<div></div>
+							<div></div>
+							<div
+								className={styles.grid_head}
+								style={{ gridRow: "span 2" }}>
+								距離
+							</div>
+							{generateDistanceButtons(distanceFilter, setDistanceFilter)}
+							<div></div>
+							<div></div>
+							<div></div>
+							<div
+								className={styles.grid_head}
+								style={{ gridRow: "span 3" }}>
+								賽道
+							</div>
+							{generatePlaceButtons(placeFilter, setPlaceFilter)}
 							{/* Search Bar */}
 							<div className={styles.grid_head}>搜尋</div>
 							<input
@@ -80,9 +172,7 @@ export default function RacePage() {
 					{/* Race Section */}
 					<div className={styles.race_grid_container}>
 						{loading ? (
-							<h1 style={{ color: "white", fontWeight: "bold" }}>
-								Loading
-							</h1>
+							<h1 style={{ color: "white", fontWeight: "bold" }}>Loading</h1>
 						) : (
 							<>
 								{raceList.G1.map(item => getItem(item))}
@@ -115,4 +205,84 @@ function RaceItem({ race, onClick }) {
 			<div>{name}</div>
 		</Link>
 	);
+}
+
+function ToggleButton({ text, color, onToggle }) {
+	const [pressed, setPressed] = useState(false);
+
+	function handlePress() {
+		onToggle();
+		setPressed(!pressed);
+	}
+
+	const button = (
+		<button
+			className={`toggle ${pressed ? "selected" : ""}`}
+			style={{ background: color }}
+			onClick={handlePress}>
+			{text}
+		</button>
+	);
+	return button;
+}
+
+function generateGradeButtons(gradeFilter, setGradeFilter) {
+	return gradeList.map((value, index) => (
+		<ToggleButton
+			text={value}
+			color={`var(--${value.toLowerCase()}-color)`}
+			onToggle={() => {
+				mylib.toggleFilterList(gradeFilter, setGradeFilter, index);
+			}}></ToggleButton>
+	));
+}
+
+function generateDirectionButtons(directionFilter, setDirectionFilter) {
+	return directionList.map((value, index) => (
+		<ToggleButton
+			text={value}
+			color="var(--toggle-color)"
+			onToggle={() => {
+				mylib.toggleFilterList(directionFilter, setDirectionFilter, index);
+			}}></ToggleButton>
+	));
+}
+
+function generateCourseButtons(courseFilter, setCourseFilter) {
+	return courseList.map((value, index) => (
+		<ToggleButton
+			text={value}
+			color="var(--toggle-color)"
+			onToggle={() => {
+				mylib.toggleFilterList(courseFilter, setCourseFilter, index);
+			}}></ToggleButton>
+	));
+}
+
+function generateDistanceButtons(distanceFilter, setDistanceFilter) {
+	return distanceList.map((value, index) => (
+		<>
+			<ToggleButton
+				text={value}
+				color="var(--toggle-color)"
+				onToggle={() => {
+					mylib.toggleFilterList(distanceFilter, setDistanceFilter, index);
+				}}></ToggleButton>
+			{value === "長距離" ? <div></div> : null}
+		</>
+	));
+}
+
+function generatePlaceButtons(placeFilter, setplaceFilter) {
+	return placeList.map((value, index) => (
+		<>
+			<ToggleButton
+				text={value}
+				color="var(--toggle-color)"
+				onToggle={() => {
+					mylib.toggleFilterList(placeFilter, setplaceFilter, index);
+				}}></ToggleButton>
+			{value === "長距離" ? <div></div> : null}
+		</>
+	));
 }
--- src/pages/support_card.js
+++ src/pages/support_card.js
@@ -1,161 +1,209 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react'
-import styles from '@/styles/SupportCardPage.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "@/styles/SupportCardPage.module.css";
 
-import * as mylib from "../lib"
+import * as mylib from "../lib";
 
-const typeList = ["速度", "持久力", "力量", "意志力", "智力", "友人", "團隊"]
-const typesEnList = ["speed", "stamina", "power", "guts", "wisdom", "friend", "group"]
-const rarityList = ["SSR", "SR", "R"]
+const typeList = ["速度", "持久力", "力量", "意志力", "智力", "友人", "團隊"];
+const typesEnList = ["speed", "stamina", "power", "guts", "wisdom", "friend", "group"];
+const rarityList = ["SSR", "SR", "R"];
 
 export default function SupportCardPage() {
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
-    // Filter
-    const [typeFilter, setTypeFilter] = useState(Array(7).fill(false));
-    const [rarityFilter, setRarityFilter] = useState(Array(3).fill(false));
-    // Load support card json file
-    const [supportList, setSupportList] = useState([])
-    async function fetchJson() {
-        setLoading(true)
-        await fetch("/data/card.json")
-        .then(res => res.json())
-        .then(res => {
-            setSupportList(res)
-        })
-        setLoading(false)
-        // console.log(supportList)
-    }
-    
-    useEffect(() => {fetchJson()}, []);
+	const router = useRouter();
+	const [loading, setLoading] = useState(true);
+	// Filter
+	const [typeFilter, setTypeFilter] = useState(Array(7).fill(false));
+	const [rarityFilter, setRarityFilter] = useState(Array(3).fill(false));
+	// Load support card json file
+	const [supportList, setSupportList] = useState([]);
+	async function fetchJson() {
+		setLoading(true);
+		await fetch("/data/card.json")
+			.then(res => res.json())
+			.then(res => {
+				setSupportList(res);
+			});
+		setLoading(false);
+		// console.log(supportList)
+	}
 
-    // Return a corresponding <SupportCardItem> from the item,
-    // or return null if the filter is not matched
-    function getItem(item) {
-        // If all toggle buttons are off, the filter is off
-        let matchType = !typeFilter.includes(true);
-        let matchRarity = !rarityFilter.includes(true);
-        // Check filters
-        typeList.forEach((value, index) => {
-            if (typeFilter[index] && value == item.type) matchType = true;
-        })
-        rarityList.forEach((value, index) => {
-            if (rarityFilter[index] && value == item.rarity) matchRarity = true;
-        })
+	useEffect(() => {
+		fetchJson();
+	}, []);
 
-        return matchType && matchRarity ? <SupportCardItem
-            card={item}
-            onClick={() => { router.push({
-                pathname: "/support_card/[uuid]",
-                query: {
-                    uuid: item.uuid
-                },
-            })}}>
-        </SupportCardItem> : null;
-    }
+	// Return a corresponding <SupportCardItem> from the item,
+	// or return null if the filter is not matched
+	function getItem(item) {
+		// If all toggle buttons are off, the filter is off
+		let matchType = !typeFilter.includes(true);
+		let matchRarity = !rarityFilter.includes(true);
+		// Check filters
+		typeList.forEach((value, index) => {
+			if (typeFilter[index] && value == item.type) matchType = true;
+		});
+		rarityList.forEach((value, index) => {
+			if (rarityFilter[index] && value == item.rarity) matchRarity = true;
+		});
 
-    return <>
-        <Head>
-            <title>支援卡一覽</title>
-        </Head>
+		return matchType && matchRarity ? (
+			<SupportCardItem
+				card={item}
+				onClick={() => {
+					router.push({
+						pathname: "/support_card/[uuid]",
+						query: {
+							uuid: item.uuid,
+						},
+					});
+				}}></SupportCardItem>
+		) : null;
+	}
 
-        <main>
-            {/* Navigation Bar */}
-            <div className="navbar">
-                <Link href="/">
-                    <img className="nav_icon" src="/icon/arrow_back.svg"></img>
-                </Link>
-                <div className="nav_title">支援卡一覽</div>
-                <div style={{flexGrow: 1}}></div> {/* Space holder */}
-                {/* <RefreshButton></RefreshButton> */}
-            </div>
-            
-            <div className={`${styles.main} main`}>
-                {/* Search Table */}
-                <div className={styles.searchTable}>
-                    <div className={styles.grid_container}>
-                        <div className={styles.grid_head}>屬性</div>
-                            {generateTypeButtons(typeFilter, setTypeFilter)}
-                        <div className={styles.grid_head}>稀有度</div>
-                            {generateRarityButtons(rarityFilter, setRarityFilter)}
-                        <div></div><div></div><div></div><div></div>
-                        {/* Search Bar */}
-                        <div className={styles.grid_head}>搜尋</div>
-                        <input type="text" className="search" placeholder="以名稱搜尋" style={{gridColumn: "2/-1"}}></input>
-                    </div>
-                </div>
+	return (
+		<>
+			<Head>
+				<title>支援卡一覽</title>
+			</Head>
 
-                <hr style={{
-                    marginTop: "20px",
-                    marginBottom: "20px"
-                }}></hr>
+			<main>
+				{/* Navigation Bar */}
+				<div className="navbar">
+					<Link href="/">
+						<img
+							className="nav_icon"
+							src="/icon/arrow_back.svg"></img>
+					</Link>
+					<div className="nav_title">支援卡一覽</div>
+					<div style={{ flexGrow: 1 }}></div> {/* Space holder */}
+					{/* <RefreshButton></RefreshButton> */}
+				</div>
 
-                {/* Support Card Section */}
-                <div className={styles.support_grid_container}>
-                    { loading 
-                    ? <h1 style={{color: "white", fontWeight: "bold"}}>loading</h1> 
-                    : supportList.map(item => getItem(item))}
-                </div>
-            </div>
-            
-        </main>
-    </>
+				<div className={`${styles.main} main`}>
+					{/* Search Table */}
+					<div className={styles.searchTable}>
+						<div className={styles.grid_container}>
+							<div className={styles.grid_head}>屬性</div>
+							{generateTypeButtons(typeFilter, setTypeFilter)}
+							<div className={styles.grid_head}>稀有度</div>
+							{generateRarityButtons(rarityFilter, setRarityFilter)}
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							{/* Search Bar */}
+							<div className={styles.grid_head}>搜尋</div>
+							<input
+								type="text"
+								className="search"
+								placeholder="以名稱搜尋"
+								style={{ gridColumn: "2/-1" }}></input>
+						</div>
+					</div>
+
+					<hr
+						style={{
+							marginTop: "20px",
+							marginBottom: "20px",
+						}}></hr>
+
+					{/* Support Card Section */}
+					<div className={styles.support_grid_container}>
+						{loading ? (
+							<h1 style={{ color: "white", fontWeight: "bold" }}>loading</h1>
+						) : (
+							supportList.map(item => getItem(item))
+						)}
+					</div>
+				</div>
+			</main>
+		</>
+	);
 }
 
-function RefreshButton({onPress}) {
-    return <button className={styles.refresh} onClick={onPress}>
-        <img className="nav_icon" src="/icon/refresh.svg"></img>
-    </button>
+function RefreshButton({ onPress }) {
+	return (
+		<button
+			className={styles.refresh}
+			onClick={onPress}>
+			<img
+				className="nav_icon"
+				src="/icon/refresh.svg"></img>
+		</button>
+	);
 }
 
-function ToggleButton({text, color, onToggle}) {
-    const [pressed, setPressed] = useState(false);
+function ToggleButton({ text, color, onToggle }) {
+	const [pressed, setPressed] = useState(false);
 
-    function handlePress() {
-        onToggle();
-        setPressed(!pressed);
-    }
+	function handlePress() {
+		onToggle();
+		setPressed(!pressed);
+	}
 
-    const button = <button
-        className={`toggle ${pressed ? "selected" : ""}`} 
-        style={{background: color}}
-        onClick={handlePress}>{text}
-    </button>
-    return button;  
+	const button = (
+		<button
+			className={`toggle ${pressed ? "selected" : ""}`}
+			style={{ background: color }}
+			onClick={handlePress}>
+			{text}
+		</button>
+	);
+	return button;
 }
 
-function SupportCardItem({card, onClick}) {
-    // return <div>{String(card)}</div>
-    const [uuid, title, rarity, type]  = [card.uuid, card.name, card.rarity, mylib.translate(card.type)];
-    const src = "/images/card/" + card.id.replace(" thumb ", "_thumb_") + ".png";
+function SupportCardItem({ card, onClick }) {
+	// return <div>{String(card)}</div>
+	const [uuid, title, rarity, type] = [
+		card.uuid,
+		card.name,
+		card.rarity,
+		mylib.translate(card.type),
+	];
+	const src = "/images/card/" + card.id.replace(" thumb ", "_thumb_") + ".png";
 
-    return <Link href={`/support_card/${uuid}`}><div className={styles.support_card_item} onClick={onClick}>
-        <img src={src} className={styles.support_card_image}></img>
-        <div className={styles.support_card_sub}>
-            <div className={styles.support_card_title}>{title}</div>
-            {/* <div className={styles.support_card_title}>{name}</div> */}
-            <div className={styles.support_card_sub_sub}>
-                <div className={styles.support_card_type} style={{backgroundImage: `url(/icon/${type}.png)`}}></div>
-                <div>{rarity}</div>
-            </div>
-        </div>
-    </div></Link>
+	return (
+		<Link href={`/support_card/${uuid}`}>
+			<div
+				className={styles.support_card_item}
+				onClick={onClick}>
+				<img
+					src={src}
+					className={styles.support_card_image}></img>
+				<div className={styles.support_card_sub}>
+					<div className={styles.support_card_title}>{title}</div>
+					{/* <div className={styles.support_card_title}>{name}</div> */}
+					<div className={styles.support_card_sub_sub}>
+						<div
+							className={styles.support_card_type}
+							style={{ backgroundImage: `url(/icon/${type}.png)` }}></div>
+						<div>{rarity}</div>
+					</div>
+				</div>
+			</div>
+		</Link>
+	);
 }
 
 function generateTypeButtons(typeFilter, setTypeFilter) {
-    return typeList.map((value, index) => <ToggleButton
-        text={value}
-        color={`var(--${typesEnList[index]}-color)`}
-        onToggle={() => { mylib.toggleFilterList(typeFilter, setTypeFilter, index) }}>
-    </ToggleButton>)
+	return typeList.map((value, index) => (
+		<ToggleButton
+			text={value}
+			color={`var(--${typesEnList[index]}-color)`}
+			onToggle={() => {
+				mylib.toggleFilterList(typeFilter, setTypeFilter, index);
+			}}></ToggleButton>
+	));
 }
 
 function generateRarityButtons(rarityFilter, setRarityFilter) {
-    return rarityList.map((value, index) => <ToggleButton
-        text={value}
-        color={`var(--${value.toLowerCase()}-color)`}
-        onToggle={() => { mylib.toggleFilterList(rarityFilter, setRarityFilter, index) }}>
-    </ToggleButton>)
+	return rarityList.map((value, index) => (
+		<ToggleButton
+			text={value}
+			color={`var(--${value.toLowerCase()}-color)`}
+			onToggle={() => {
+				mylib.toggleFilterList(rarityFilter, setRarityFilter, index);
+			}}></ToggleButton>
+	));
 }
