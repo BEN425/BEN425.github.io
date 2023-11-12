e10f3c7876f8a92f9f7b68223755fa0e1a9abf42
BEN425 <78299515+BEN425@users.noreply.github.com>
2023-11-13 00:24:32 +0800

    modified:   public/data/race.json modified:   src/pages/index.js modified:   src/styles/Home.module.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,14 +29,13 @@ export default function Home() {
 				{/* Top Row */}
 				<div className={`${styles.top_row} row`}>
 					<div className={`${styles.icon} ${styles.col}`}></div>
-					<div className={`${styles.title} ${styles.col}`}>
-						Uma Site
-					</div>
-					<div className={`${styles.intro} ${styles.col}`}>
+					<div className={`${styles.title} ${styles.col}`}>Uma Site</div>
+					<div
+						className={`${styles.intro} ${styles.col}`}
+						style={{ display: "none" }}>
 						<div className={styles.sns_row}>
 							<div className={styles.about}>簡介</div>
-							<div style={{ flexGrow: 1 }}></div>{" "}
-							{/* Placeholder */}
+							<div style={{ flexGrow: 1 }}></div> {/* Placeholder */}
 							<button
 								className={styles.sns_button}
 								id={styles.gamer_button}>
@@ -72,6 +71,42 @@ export default function Home() {
 							image="/icon/Race.png"
 							link="/race"></GridItem>
 						{/* <GridItem title="測試" link="/test"></GridItem> */}
+					</div>
+				</div>
+
+				<div
+					className="main"
+					style={{ marginTop: "20px" }}>
+					<div className={styles.grid_title_container}>
+						<div className={styles.title_line}></div>
+						<div className={styles.grid_title}>關於網站</div>
+						<div className={styles.title_line}></div>
+					</div>
+
+					<div className={styles.text}>
+						此網站是作者個人興趣加上學習目的才製作出來的網站，會不定期更新及修正錯誤。
+					</div>
+					<div className={styles.text}>資料來源：</div>
+					<div>
+						<a
+							href="https://wiki.biligame.com/umamusume/%E9%A6%96%E9%A1%B5"
+							className={styles.link}>
+							賽馬娘 BiliWiki
+						</a>
+					</div>
+					<div>
+						<a
+							href="https://xn--gck1f423k.xn--1bvt37a.tools/"
+							className={styles.link}>
+							Utools
+						</a>
+					</div>
+					<div>
+						<a
+							href="https://zh.moegirl.org.cn/zh-tw/%E8%B5%9B%E9%A9%AC%E5%A8%98_Pretty_Derby/%E8%AF%91%E5%90%8D%E5%AF%B9%E7%85%A7%E8%A1%A8"
+							className={styles.link}>
+							萌娘百科
+						</a>
 					</div>
 				</div>
 			</main>
